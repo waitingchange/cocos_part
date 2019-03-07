49a463e9960a6af310d6551a4471a0129f7a0cd0
change <waiting0313@gmail.com>
2019-03-07 22:09:35 +0800

    remove useless library

--- HelloC/cocos2d/cocos/base/ccConfig.h
+++ HelloC/cocos2d/cocos/base/ccConfig.h
@@ -133,7 +133,7 @@ THE SOFTWARE.
  * Only valid for cocos2d-mac. Not supported on cocos2d-ios.
  */
 #ifndef CC_DIRECTOR_MAC_USE_DISPLAY_LINK_THREAD
-#define CC_DIRECTOR_MAC_USE_DISPLAY_LINK_THREAD 1
+#define CC_DIRECTOR_MAC_USE_DISPLAY_LINK_THREAD 0
 #endif
 
 /** @def CC_NODE_RENDER_SUBPIXEL
@@ -254,13 +254,13 @@ THE SOFTWARE.
 
 /** Use physics integration API. */
 #ifndef CC_USE_PHYSICS
-#define CC_USE_PHYSICS 1
+#define CC_USE_PHYSICS 0
 #endif
 
 #if (CC_USE_PHYSICS)
 /** Use chipmunk physics 2d engine. */
 #ifndef CC_ENABLE_CHIPMUNK_INTEGRATION
-#define CC_ENABLE_CHIPMUNK_INTEGRATION 1
+#define CC_ENABLE_CHIPMUNK_INTEGRATION 0
 #endif
 
 /** or use box2d physics 2d engine. */
@@ -272,20 +272,20 @@ THE SOFTWARE.
 /** Use 3d physics integration API. */
 #ifndef CC_USE_3D_PHYSICS
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_LINUX /*|| CC_TARGET_PLATFORM == CC_PLATFORM_WINRT*/)
-#define CC_USE_3D_PHYSICS 1
+#define CC_USE_3D_PHYSICS 0
 #endif
 #endif
 
 #if (CC_USE_3D_PHYSICS)
 /** Use bullet physics engine. */
 #ifndef CC_ENABLE_BULLET_INTEGRATION
-#define CC_ENABLE_BULLET_INTEGRATION 1
+#define CC_ENABLE_BULLET_INTEGRATION 0
 #endif
 #endif
 
 /** Use 3D navigation API */
 #ifndef CC_USE_NAVMESH
-#define CC_USE_NAVMESH 1
+#define CC_USE_NAVMESH 0
 #endif
 
 /** Use culling or not. */
@@ -315,7 +315,7 @@ THE SOFTWARE.
  */
 #ifndef CC_USE_WEBP
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
-#define CC_USE_WEBP  1
+#define CC_USE_WEBP  0
 #endif
 #endif // CC_USE_WEBP
 
@@ -323,7 +323,7 @@ THE SOFTWARE.
  */
 #ifndef CC_USE_WIC
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-#define CC_USE_WIC  1
+#define CC_USE_WIC  0
 #undef CC_USE_TIFF
 #undef CC_USE_JPEG
 #undef CC_USE_PNG
